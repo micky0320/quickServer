8ab275a2393378a44dbf8c28a398e036bb67ad8a
youyuan <youyuan702@pingan.com.cn>
2019-09-26 10:29:00 +0800

    更新server文件

--- server/server.js
+++ server/server.js
@@ -7,34 +7,49 @@ var c = require('child_process');
 var fs = require('fs');
 var path = require('path');
 var livereload = require('livereload');
-
+var mine = require('./mine').types;
 var filePath = './src/html';
 
 //创建服务器
 http.createServer(function(request,response) {
     //解析请求，包括文件名
     var pathname= url.parse(request.url).pathname;
+    var realPath = path.join("./", pathname);
+    console.log(realPath);
+    var ext = path.extname(realPath);
+    ext = ext ? ext.slice(1) : 'unknown';
     //输出请求的文件名
     // console.log("Request for "+ pathname + "  received.");
-
+    // console.log(request,response, 'res------')
     //从文件系统中都去请求的文件内容
-    fs.readFile(pathname.substr(1),function(err, data) {
-        if(err) {
-            // console.log(err);
-            //HTTP 状态码 404 ： NOT FOUND
-            //Content Type:text/plain
-            response.writeHead(404,{'Content-Type': 'text/html'});
-        }
-        else {
-            //HTTP 状态码 200 ： OK
-            //Content Type:text/plain
-            response.writeHead(200,{'Content-Type': 'text/html'});
+    fs.exists(realPath, function (exists) {
+        console.log(exists,'ex-----')
+        if (!exists) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
 
-            //写会相应内容
-            response.write(data.toString());
+            response.write("This request URL " + pathname + " was not found on this server.");
+            response.end();
+        } else {
+            fs.readFile(realPath, function (err, data) {
+                if (err) {
+                    response.writeHead(500, { 'Content-Type': 'text/plain' });
+                    response.end(err);
+                }
+                else {
+                    //HTTP 状态码 200 ： OK
+                    //Content Type:text/plain
+                    var contentType = mine[ext] || "text/plain";
+                    response.writeHead(200, { 'Content-Type': contentType });
+
+                    //写会相应内容
+                    response.write(data.toString());
+                    //发送响应数据
+                    response.end();
+                }
+            });
         }
-        //发送响应数据
-        response.end();
     });
 }).listen(8081);
 
